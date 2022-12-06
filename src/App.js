345765b1b34ae661a9a1dc50029e40299f90c78c
AkhileshS123 <akhilesh.sivaganesan@gmail.com>
2022-12-06 18:03:19 -0500

    Added file viewer and dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
+import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import { Logger } from "logging-library";
+import FileViewer from "react-file-viewer";
+import { CustomErrorComponent } from "custom-error";
+const file = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFquUdIOtcM_jAE5BPAcBz4gY0UsdI-0XywAHim3CPKO_T-h3ZGyRrsKxaZHEb29VnP1U&usqp=CAU";
+const type = "png";
+const App = () => {
+  const [view, setView] = useState(false);
 
-function App() {
+  const handleView = () => {
+    setView(!view);
+  };
+
+  const onError = (e) => {
+    Logger.logError(e, "error in file-viewer");
+  };
+
   return (
-    <div className="App">
+    <>
       <h1>Hello BeyondMD!</h1>
-    </div>
+      <Button variant="contained" onClick={handleView}>View</Button>
+      {view && (
+        <FileViewer
+          fileType={type}
+          filePath={file}
+          errorComponent={CustomErrorComponent}
+          onError={onError}
+        />
+      )}
+    </>
   );
-}
+};
+
 
 export default App;
