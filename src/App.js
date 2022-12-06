e310da810dd9afc5a2fd5ffc82e881afcc37c7b6
AkhileshS123 <akhilesh.sivaganesan@gmail.com>
2022-12-06 18:15:53 -0500

    Replaced file viewer with iframe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import Button from '@mui/material/Button';
 import React, { useState } from "react";
-import { Logger } from "logging-library";
-import FileViewer from "react-file-viewer";
-import { CustomErrorComponent } from "custom-error";
-const file = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFquUdIOtcM_jAE5BPAcBz4gY0UsdI-0XywAHim3CPKO_T-h3ZGyRrsKxaZHEb29VnP1U&usqp=CAU";
-const type = "png";
 const App = () => {
   const [view, setView] = useState(false);
 
@@ -13,22 +8,13 @@ const App = () => {
     setView(!view);
   };
 
-  const onError = (e) => {
-    Logger.logError(e, "error in file-viewer");
-  };
-
   return (
     <>
       <h1>Hello BeyondMD!</h1>
-      <Button variant="contained" onClick={handleView}>View</Button>
+      <Button variant="contained" onClick={handleView}>View Resume</Button>
       {view && (
-        <FileViewer
-          fileType={type}
-          filePath={file}
-          errorComponent={CustomErrorComponent}
-          onError={onError}
-        />
-      )}
+        <iframe src="https://drive.google.com/file/d/1sBDGIaBdA-dQf7ro__sK9icdCicEhXuM/preview" width="400" height="500" allow="autoplay"></iframe>
+      )}      
     </>
   );
 };
