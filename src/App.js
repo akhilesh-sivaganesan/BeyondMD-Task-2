94ac27734c8267d83d7a5df93e4dc3a5bb0b6e2c
AkhileshS123 <akhilesh.sivaganesan@gmail.com>
2022-12-06 21:12:17 -0500

    Hello BeyondMD with Resume and axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import axios from 'axios';
 import React, { useState } from "react";
 const App = () => {
   const [view, setView] = useState(false);
@@ -7,15 +13,36 @@ const App = () => {
   const handleView = () => {
     setView(!view);
   };
+  
+  //const axios = require("axios");
+
+  const options = {
+    method: 'GET',
+    url: 'https://open-weather13.p.rapidapi.com/city/landon',
+    headers: {
+      'X-RapidAPI-Key': '92994eff54mshe6dc13d4b587db3p1201dajsn612ea6fb83bb',
+      'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data)
+  }).catch(function (error) {
+    console.log(error);
+  });
+
+
 
   return (
-    <>
-      <h1>Hello BeyondMD!</h1>
-      <Button variant="contained" onClick={handleView}>View Resume</Button>
+    <div class="column">
+      <Typography variant="h1" component="h2">
+        Hello BeyondMD!
+      </Typography>;
+      <Button sx={{ width: 200, padding: 1, margin: 2 }} variant="contained" onClick={handleView}>View Resume</Button>
       {view && (
-        <iframe src="https://drive.google.com/file/d/1sBDGIaBdA-dQf7ro__sK9icdCicEhXuM/preview" width="400" height="500" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/1sBDGIaBdA-dQf7ro__sK9icdCicEhXuM/preview" class="resume" allow="autoplay"></iframe>
       )}      
-    </>
+    </div>
   );
 };
 
